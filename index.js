64e0fbc91d1b5cdede2d8812e6dbe98ffbc240f0
iamcal77 <+>
2023-06-26 14:09:47 +0300

    assignment done

---
+++ index.js
@@ -0,0 +1,17 @@
+// index.js
+
+// Function expression
+const divide = function() {
+    return 2000 / 100;
+  };
+  
+  // Arrow function
+  const square = (x) => {
+    return x * x;
+  };
+  
+  // Arrow function
+  const add = (a, b) => {
+    return a + b;
+  };
+  
